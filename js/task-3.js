8e5b2b7f869e777d32a64b7ad3b9e1439407da85
ValeriiaSai <kolyadavaleri@gmail.com>
2024-12-01 19:22:07 +0200

    ./js
    ./

--- js/task-3.js
+++ js/task-3.js
@@ -8,4 +8,3 @@ function getElementWidth(content, padding, border) {
 getElementWidth('50px', '8px', '4px');
 getElementWidth('60px', '12px', '8.5px');
 getElementWidth('200px', '0px', '0px');
-//getElementWidth(`100px`, `10px`, `10px`);
